393964848600770cb0e257d121143616cf120b75
Francesco Rubino <francesco@superkaban.xyz>
2023-03-13 21:28:01 +0000

    Revert "Added clone and copy for PnPs"
    This reverts commit eecd81fd4d80d527689829dc5f77da664ef77a29.

--- src/snps.rs
+++ src/snps.rs
@@ -38,7 +38,7 @@ pub trait CalculatePnPs {
     }
 }
 
-#[derive(Debug, Default, Serialize, Deserialize, Clone, Copy)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct PnPs {
     pub uid: Uuid,
     pub coverage: u32,
